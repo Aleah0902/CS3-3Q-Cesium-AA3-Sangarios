80bfca21684ba5cab0a0b4e92bee30f2ef01ba14
Aleah0902 <aygsangarios22@src.pshs.edu.ph>
2025-03-05 15:37:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function confirmMessage() {
-    let messageConfirm = prompt ('Enter a message to verify: ');
+    let messageConfirm = prompt('Enter a message to verify: ');
 
-    if (messageConfirm.indexOf('AI') >= 0 || messageConfirm.indexOf('aI') >= 0 || messageConfirm.indexOf('Ai' >= 0) {
+    if (messageConfirm.indexOf('AI') >= 0 || messageConfirm.indexOf('aI') >= 0 || messageConfirm.indexOf('Ai' >= 0)) {
     	document.getElementById('result').innerHTML = "The message " + messageConfirm + " is tampered!";
 	}	
     if ((messageConfirm.indexOf('FR') >= 0) && (messageConfirm.indexOf('AI') >= 0) || messageConfirm.indexOf('FR') >= 0) {
