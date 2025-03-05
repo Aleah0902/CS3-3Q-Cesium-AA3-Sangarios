a925219ba01b326f425e4250e9afa8ed756480ab
Aleah0902 <aygsangarios22@src.pshs.edu.ph>
2025-03-05 15:31:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function confirmMessage() {
     let messageConfirm = prompt ('Enter a message to verify: ');
 
     if (messageConfirm.indexOf('AI') >= 0 || messageConfirm.indexOf('aI') >= 0 || messageConfirm.indexOf('Ai' >= 0) {
-    	document.getElementById('result').innerHTML = "The message" + messageConfirm + "is tampered!";
+    	document.getElementById('result').innerHTML = "The message " + messageConfirm + " is tampered!";
 	}	
     if ((messageConfirm.indexOf('FR') >= 0) && (messageConfirm.indexOf('AI') >= 0) || messageConfirm.indexOf('FR') >= 0) {
-	document.getElementById('result').innerHTML = "The message" + messageConfirm + "is legitimate!";
+	document.getElementById('result').innerHTML = "The message " + messageConfirm + " is legitimate!";
 	}
     else {
-	document.getElementById('result').innerHTML = 'The message ${messageConfirm} is yet to be encoded!' ;
+	document.getElementById('result').innerHTML = "The message " + messageConfirm + " is yet to be encoded!";
     }
 }
